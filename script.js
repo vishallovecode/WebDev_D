96c523d5a518340c5f482859bb4323d260da6a54
Vishal Sharma <785vishal443@gmail.com>
2025-01-07 23:48:09 +0530

    Lecture 1

---
+++ script.js
@@ -0,0 +1,50 @@
+// Var Keyword
+var firstName = "Vishal";
+
+var firstName; // declaration
+firstName = "Vishal" // iniatilization
+
+var lastName = 'Sharma';
+var age = 20;
+
+console.log(firstName , lastName , age)
+
+//let keyword
+
+let  college = "Newton school of technology"
+let semester = 2;
+
+// const keyword
+const country = 'India'
+
+
+// declaration  
+// initilization 
+// not changeballe value
+
+
+// var variable can be re-declare and re-assign 
+
+var a =30;
+console.log('A value=>' ,  a)
+
+var a  =40 // re-declaring a
+
+a = 100 ;  // re-assign
+
+console.log('Update A=>' , a)
+
+
+// let variable cannot be re-declare  , but we can re-assign the value
+
+
+let aa =30;
+console.log('AA value=>' ,  aa)
+
+// let aa  = 40 // re-declaring a
+
+aa = 1000 ;  // re-assign
+
+console.log('Update A=>' ,aa)
+
+// constant variable cannot be declare and re-assign
